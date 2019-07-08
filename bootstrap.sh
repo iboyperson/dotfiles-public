0fba985e98f7c437e1935db242f31de0eae70301
iboyperson <tjquillan@gmail.com>
2019-07-07 19:33:11 -0700

    Update public dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,23 @@ function install_reqs() {
     fi
 }
 
-ROOT_DIR="$(dirname $0)"
+ROOT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+GOAL_DIR="/dotfiles"
+
+# Ensure ansible is being run from a pre-established directory
+# If it is not move itself then run bootstrap from the new location
+if [[ $ROOT_DIR != $GOAL_DIR ]]; then
+    if [[ ! -d $GOAL_DIR ]]; then
+        mkdir -p $GOAL_DIR
+    fi
+
+    for x in $ROOT_DIR/* $ROOT_DIR/.[!.]* $ROOT_DIR/..?*; do
+        if [ -e "$x" ]; then mv -- "$x" $GOAL_DIR; fi
+    done
+
+    trap "sh $GOAL_DIR/bootstrap.sh;rm -r $ROOT_DIR;" EXIT
+    exit
+fi
 
 echo "Installing requirements to bootstrap..."
 install_reqs
